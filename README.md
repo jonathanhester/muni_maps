36fcaa4d955b693a74c2b787905a486996548d76
Jonathan Hester <hester.jonathan@gmail.com>
2013-12-23 09:44:05 -0800

    sometimes location was taking long on my phone so only wait 3 seconds and then load the city center and zoom out so user can easily move to desired location

--- README.md
+++ README.md
@@ -2,16 +2,18 @@ Muni Maps
 ====================
 Hosted at http://muni.jonathanhester.com
 
-This is a quick implementation of a web app that uses NextBus's API
-([Nextbus](http://www.nextbus.com/xmlFeedDocs/NextBusXMLFeed.pdf)) to display
-predicted sf muni arrival times at nearby bus stops on a google map.
-I wanted the least cumbersome way possible to lookup nearby arrival times.
-It uses the browser's Geolocation capability to determine the initial location.
+This is a quick implementation of a web app that uses
+([NextBus's API](http://www.nextbus.com/xmlFeedDocs/NextBusXMLFeed.pdf)) to display
+predicted SF Muni arrival times at nearby bus stops on a google map.
+I wanted the least cumbersome way to lookup nearby arrival times.
+It uses the browser's Geolocation capability to determine the initial location. If
+the browser doesn't return a location within 3 seconds, it defaults to the center
+of San Francisco.
 
 Motivation
 ---------------
 This is to solve the use case where you are familiar with the surrounding bus
-lines and just want to know the nearby arrival times.  I found the Android
+lines and just want to know their arrival times.  I found the Android
 ([Muni+ App](http://muniapp.us/)) that
 [SFMTA recommends](http://www.sfmta.com/getting-around/transit/mobile-apps) unsatisfactory.
 The mobile version of [Nextbus](http://www.nextbus.com/) has the same idea but
@@ -24,14 +26,17 @@ Implementation
 This really should be a native app to get the improved geolocation accuracy and Google Maps
 performance.
 
+### Front-end
+
 The frontend is a fullscreen google map with javascript that
-* geolocates the user(note this requires user to explicitly allow location permission),
-* queries the backend to get a list of nearby stops with their corresponding bus routes
-* queries Nextbus's API directly with all the route/stop combos
-* displays Markers and InfoWindows for each bus stop
+
+1. geolocates the user (note this requires user to explicitly allow location permission),
+2. queries the backend to get a list of nearby stops with their corresponding bus routes
+3. queries Nextbus's API directly with all the route/stop combos from #2
+4. displays Markers and InfoWindows for each bus stop
 
 Whenever the user stops dragging the map, it will query with the new map center.
-Future TODOs would be to add refresh functionality (or poll) and be smarter about
+A future TODO would be to add refresh functionality (or poll) and be smarter about
 when and what data to load.  If the user is doing several drags to move the map
 far, they likely don't care about the information being requested between each drag.
 Also, if the user moves the map to a location that already has "up-to-date" data,
@@ -42,9 +47,16 @@ to re-sync the times.
 
 Its dependencies include jQuery and Google Maps JS API
 
+
+### Back-end
+
 The backend is a Sinatra app responsible for returning nearby bus stops and all the routes
-that use the stop.
-This information probably won't change much so it shouldn't need to be updated too frequently.
+that use the stop.  This was my first Sinatra app but I found it fitting given the simplicity
+of the server's responsibilities.  It uses 2 Nextbus API endpoints to load the data into 4 models.
+
+Besides the query it needs to serve, it also has a function that refreshes all the data.
+Given that bus routes/stops shouldn't change much, it shouldn't need to be updated too frequently.
+This function should be wrapped in a script and added as a cron job.
 It takes a couple minutes to refresh and while that could be optimized, you'd have to watch
 out for Nextbus's API limits.
 
@@ -58,5 +70,7 @@ to cover more cases and the code that draws the markers and infowindows.
 The QUnit tests can be seen at http://muni.jonathanhester.com/test.html
 
 
+Author
+---------------
 Jonathan Hester
 http://lnkd.in/bDPdcGc
--- public/app.js
+++ public/app.js
@@ -4,47 +4,76 @@ function log(message) {
 }
 
 google.maps.event.addDomListener(window, 'load', function () {
+    var alreadyLoading = false;
+    var SF_DEFAULT_LATITUDE = 37.7833;
+    var SF_DEFAULT_LONGITUDE = -122.4167;
+
+    function loadMuniMaps(lat, lng, zoom) {
+        if (alreadyLoading)
+            return;
+        else
+            alreadyLoading = true;
+
+        var GOOGLE_MAP_OPTIONS = {
+            styles:[
+                {
+                    featureType:"poi",
+                    stylers:[
+                        { visibility:"off" }
+                    ]
+                }
+            ]
+        };
+
+        // maybe we want to customize based on device, resolution, etc
+        function getGoogleMapOptions() {
+            return GOOGLE_MAP_OPTIONS;
+        }
+
+        var mapOptions = $.extend(getGoogleMapOptions(), {
+            center:new google.maps.LatLng(lat, lng),
+            zoom:zoom
+        });
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+
+        var muniMap = new MuniMap(map, lat, lng);
+        muniMap.loadPredictions(lat, lng, muniMap.draw);
+
+        //may want to make this smarter, like it waits 1 second after the
+        //drag before initiating the load. On mobile, it might be common
+        // to do a bunch of little drags to get where you want
+        google.maps.event.addListener(map, 'dragend', function () {
+            var center = map.getBounds().getCenter();
+            muniMap.loadPredictions(center.lat(), center.lng(), muniMap.draw);
+        });
+
+    }
+
+    function loadDefaultPosition() {
+        loadMuniMaps(SF_DEFAULT_LATITUDE, SF_DEFAULT_LONGITUDE, 14);
+    }
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-            var GOOGLE_MAP_OPTIONS = {
-                styles:[
-                    {
-                        featureType:"poi",
-                        stylers:[
-                            { visibility:"off" }
-                        ]
-                    }
-                ]
-            };
-
-            // maybe we want to customize based on device, resolution, etc
-            function getGoogleMapOptions() {
-                return GOOGLE_MAP_OPTIONS;
-            }
-
-            var mapOptions = $.extend(getGoogleMapOptions(), {
-                center:new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-                zoom:17
-            });
-            var map = new google.maps.Map(document.getElementById("map-canvas"),
-                mapOptions);
-
-            var muniMap = new MuniMap(map, position.coords.latitude, position.coords.longitude);
-            muniMap.loadPredictions(position.coords.latitude, position.coords.longitude, muniMap.draw);
-
-            //may want to make this smarter, like it waits 1 second after the
-            //drag before initiating the load. On mobile, it might be common
-            // to do a bunch of little drags to get where you want
-            google.maps.event.addListener(map, 'dragend', function () {
-                var center = map.getBounds().getCenter();
-                muniMap.loadPredictions(center.lat(), center.lng(), muniMap.draw);
-            });
-
+            loadMuniMaps(position.coords.latitude, position.coords.longitude, 16);
         }, function (positionError) {
             log("There was an error getting position.");
+            loadDefaultPosition();
         });
     } else {
         log("Geolocation is not supported by this browser.");
+        loadDefaultPosition();
     }
+
+
+    //give the browser 3 seconds to come up with a location for the user,
+    //otherwise, just choose center of SF and zoom out a little bit
+    //so they can easily find it themselves
+    setTimeout(function() {
+        loadDefaultPosition();
+    },3000);
+
+
 });
 
